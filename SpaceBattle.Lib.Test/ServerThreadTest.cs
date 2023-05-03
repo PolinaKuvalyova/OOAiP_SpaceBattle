f7552c263b8b3a6228e842b2b11fc68e2ecc4014
Polina <polina.andreevna.4709@mail.ru>
2023-05-03 18:25:03 +0600

    add SoftStopTest!

--- SpaceBattle.Lib.Test/ServerThreadTest.cs
+++ SpaceBattle.Lib.Test/ServerThreadTest.cs
@@ -223,7 +223,6 @@ public class ServerThreadTest
         ServerThread stSoftStop = Hwdtech.IoC.Resolve<ServerThread>("Get Thread by id", 8);
         SoftStop softStop = new(stSoftStop);
 
-
         Hwdtech.IoC.Resolve<object>("Create And Start Thread", 7, () => {IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", scope).Execute();});
         IReceiver receiver =( Hwdtech.IoC.Resolve<ServerThread>("Get Thread by id", 7)).receiver;
         ServerThread st_stop = new(receiver);
