4d6b8b7130323daeb3ed1e1c0ca744473098f775
Polina <polina.andreevna.4709@mail.ru>
2023-05-05 13:00:44 +0600

    add test

--- SpaceBattle.Lib.Test/ServerThreadTest.cs
+++ SpaceBattle.Lib.Test/ServerThreadTest.cs
@@ -239,6 +239,7 @@ public class ServerThreadTest
     {
         var scope = IoCInit();
 
+
         Hwdtech.IoC.Resolve<object>("Create And Start Thread", 8);
         ServerThread stSoftStop = Hwdtech.IoC.Resolve<ServerThread>("Get Thread by id", 8);
         SoftStop softStop = new(stSoftStop);
