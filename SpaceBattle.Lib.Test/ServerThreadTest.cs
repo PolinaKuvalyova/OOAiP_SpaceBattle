72fbdd68ded1c0109588fabec2ed202bec9b84e3
Polina <polina.andreevna.4709@mail.ru>
2023-05-14 15:51:14 +0600

    ??codecov name classes

--- SpaceBattle.Lib.Test/ServerThreadTest.cs
+++ SpaceBattle.Lib.Test/ServerThreadTest.cs
@@ -259,7 +259,6 @@ public class ServerThreadTest
         r.Setup(r => r.IsEmpty()).Returns(() => queue.Count == 0);
 
         ServerThread thread = new(r.Object);
-
         SoftStop s = new(thread);
 
         s.Get()();
