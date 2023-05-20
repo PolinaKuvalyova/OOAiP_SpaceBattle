11ae708d6d035b3a4a7cd058b683516e0d544440
Polina <polina.andreevna.4709@mail.ru>
2023-05-20 20:47:10 +0600

    modify .csproj

--- SpaceBattle.Lib/Startup.cs
+++ SpaceBattle.Lib/Startup.cs
@@ -25,49 +25,4 @@ public class Startup
 
         services.AddSingleton(new SwaggerOptions());
     }
-
-    public void Configure(IApplicationBuilder app)
-    {
-        app.UseMiddleware<SwaggerMiddleware>();
-        app.UseSwaggerUI();
-
-        app.UseServiceModel(builder =>
-        {
-            var readerQuoates = new XmlDictionaryReaderQuotas
-            {
-                MaxBytesPerRead = 4096,
-                MaxDepth = 32,
-                MaxArrayLength = 16384,
-                MaxStringContentLength = 16384,
-                MaxNameTableCharCount = 16384
-            };
-
-            builder.AddService<WebApi>();
-            builder.AddServiceWebEndpoint<WebApi, IWebApi>(new WebHttpBinding
-            {
-                MaxReceivedMessageSize = 5242880,
-                MaxBufferSize = 65536,
-                ReaderQuotas = readerQuoates
-            }, "api", behavior =>
-            {
-                behavior.HelpEnabled = true;
-                behavior.AutomaticFormatSelectionEnabled = true;
-            });
-
-            builder.AddServiceWebEndpoint<WebApi, IWebApi>(new WebHttpBinding
-            {
-                Security = new WebHttpSecurity
-                {
-                    Mode = WebHttpSecurityMode.Transport
-                },
-                MaxReceivedMessageSize = 5242880,
-                MaxBufferSize = 65536,
-                ReaderQuotas = readerQuoates
-            }, "api", behavior =>
-            {
-                behavior.HelpEnabled = true;
-                behavior.AutomaticFormatSelectionEnabled = true;
-            });
-        });
-    }
 }
