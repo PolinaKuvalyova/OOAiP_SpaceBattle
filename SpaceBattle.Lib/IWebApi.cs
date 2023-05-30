c8149707f9fac89479d6aac6f6ef5cdbebd8524e
Polina <polina.andreevna.4709@mail.ru>
2023-05-30 13:56:12 +0600

    add Strategy

---
+++ SpaceBattle.Lib/IWebApi.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using CoreWCF;
+using CoreWCF.OpenApi.Attributes;
+using CoreWCF.Web;
+
+
+namespace SpaceBattle.Lib;
+
+[ServiceContract]
+public interface IWebApi
+{
+    [OperationContract]
+    [WebInvoke(Method = "POST", UriTemplate = "/body")]
+    [OpenApiTag("Tag")]
+    [OpenApiResponse(ContentTypes = new[] { "application/json", "text/xml" }, Description = "Success", StatusCode = HttpStatusCode.OK, Type = typeof(Contract)) ]
+    Contract BodyEcho(
+        [OpenApiParameter(ContentTypes = new[] { "application/json", "text/xml" }, Description = "param description.")] Contract param);
+}
---
+++ SpaceBattle.Lib/WebApi.cs
@@ -0,0 +1,15 @@
+﻿using CoreWCF;
+
+namespace SpaceBattle.Lib;
+
+[ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+public class WebApi : IWebApi
+{
+    public Contract BodyEcho(Contract param){
+        foreach(object obj in param.Registration.Entries)
+        {
+            Console.WriteLine(obj);
+        }
+        return param;
+    }
+}
